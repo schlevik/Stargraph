0dbe8c3415dce8b86b824b6aad43191e2d4c48a7
viktor <viktor@schlegel-online.de>
2018-01-05 15:40:28 +0100

    ... Created "extend" in KBCore and its implementations.
    forgot a file :(

--- stargraph-model/src/main/java/net/stargraph/data/DataProviderFactory.java
+++ stargraph-model/src/main/java/net/stargraph/data/DataProviderFactory.java
@@ -12,10 +12,10 @@ package net.stargraph.data;
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -29,8 +29,12 @@ package net.stargraph.data;
 import net.stargraph.data.processor.Holder;
 import net.stargraph.model.KBId;
 
+import java.util.List;
+
 public interface DataProviderFactory {
 
     DataProvider<? extends Holder> create(KBId kbId);
+
+    DataProvider<? extends Holder> create(KBId kbId, List data);
 
 }
